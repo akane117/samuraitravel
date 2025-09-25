22a54187cbe3e156b4a79d4d2e95eb0ca55384ac
akane117 <usui.akane.0117@gmail.com>
2025-09-25 16:12:21 +0900

    5 commit

--- src/main/java/com/example/samuraitravel/security/WebSecurityConfig.java
+++ src/main/java/com/example/samuraitravel/security/WebSecurityConfig.java
@@ -18,7 +18,7 @@ public class WebSecurityConfig {
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
 		http
 		.authorizeHttpRequests((requests) -> requests
-				.requestMatchers("/css/**","/images/**","/js/**","/storage/**","/").permitAll()
+				.requestMatchers("/css/**","/images/**","/js/**","/storage/**","/","/signup/**").permitAll()
 				.anyRequest().authenticated()  // 上記以外のURLはログインが必要（会員または管理者のどちらでもOK）
 				)
 		.formLogin((form) -> form   
