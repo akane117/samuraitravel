483c66697d7b8c1d2b5da653b964a297348935f3
akane117 <usui.akane.0117@gmail.com>
2025-10-20 22:15:58 +0900

    10 commit

--- src/main/java/com/example/samuraitravel/controller/AdminHouseController.java
+++ src/main/java/com/example/samuraitravel/controller/AdminHouseController.java
@@ -8,13 +8,19 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.example.samuraitravel.entity.House;
+import com.example.samuraitravel.form.HouseEditForm;
+import com.example.samuraitravel.form.HouseRegisterForm;
 import com.example.samuraitravel.service.HouseService;
 
 @Controller
@@ -61,5 +67,51 @@ public class AdminHouseController {
 
 	        return "admin/houses/show";
 	    }   
+	 
+	 @GetMapping("/register")
+	 public String register(Model model) {
+		 model.addAttribute("houseRegisterForm", new HouseRegisterForm());
+		 
+		 return "admin/houses/register";
+	 }
+	 
+	 @PostMapping("/create")
+	    public String create(@ModelAttribute @Validated HouseRegisterForm houseRegisterForm,
+	                         BindingResult bindingResult,
+	                         RedirectAttributes redirectAttributes,
+	                         Model model)
+	    {
+	        if (bindingResult.hasErrors()) {
+	            model.addAttribute("houseRegisterForm", houseRegisterForm);
+
+	            return "admin/houses/register";
+	        }
+
+	        houseService.createHouse(houseRegisterForm);
+	        redirectAttributes.addFlashAttribute("successMessage", "民宿を登録しました。");
+
+	        return "redirect:/admin/houses";
+	    }    
+	 
+
+	    @GetMapping("/{id}/edit")
+	    public String edit(@PathVariable(name = "id") Integer id, RedirectAttributes redirectAttributes, Model model) {
+	        Optional<House> optionalHouse = houseService.findHouseById(id);
+
+	        if (optionalHouse.isEmpty()) {
+	            redirectAttributes.addFlashAttribute("errorMessage", "民宿が存在しません。");
+
+	            return "redirect:/admin/houses";
+	        }
+
+	        House house = optionalHouse.get();
+	        HouseEditForm houseEditForm = new HouseEditForm(house.getName(), null, house.getDescription(), house.getPrice(), house.getCapacity(), house.getPostalCode(), house.getAddress(), house.getPhoneNumber());
+
+	        model.addAttribute("house", house);
+	        model.addAttribute("houseEditForm", houseEditForm);
+
+	        return "admin/houses/edit";
+	    } 
+	    
 	
 }
